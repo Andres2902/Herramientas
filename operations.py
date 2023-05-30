f6894bb7c18a4f6532c6b27d30ac59f8e642b864
andresfbejarano <131776631+andresfbejarano@users.noreply.github.com>
2023-05-30 02:04:12 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -2,3 +2,8 @@ def get_results(word):
     ascii_values = get_ascii(word)
     binary_values = get_binary(word)
     return {'ascii': ascii_values, 'binary': binary_values}
+def obtener_ascii(palabra):
+    valores_ascii = []
+    for caracter in palabra:
+        valores_ascii.append(ord(caracter))
+    return valores_ascii
