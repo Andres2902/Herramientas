e21c98bf5eeddd0b226b8e2b5de66f113199f827
Andres2902 <72894951+Andres2902@users.noreply.github.com>
2023-05-30 02:26:55 -0500

    Update base.py
    se unio todo el codigo en uno solo

--- base.py
+++ base.py
@@ -1,16 +1,39 @@
-from operations import get_ascci, get_binary, get_results
 import sys
-menu = int(input('Menu\n======\n1. Character\n2. Word'))
 
-if (menu == 1):
-    char = input('Enter a character: ')
-    word = char
-elif (menu == 2):
+def get_results(word):
+    ascii_values = get_ascii(word)
+    binary_values = get_binary(word)
+    return {'ascii': ascii_values, 'binary': binary_values}
+
+def get_ascii(word):
+    valores_ascii = []
+    for caracter in word:
+        valores_ascii.append(ord(caracter))
+    return valores_ascii
+
+def get_binary(word):
+    binary_values = []
+    for character in word:
+        binary_values.append(bin(ord(character))[2:])
+    return binary_values
+
+menu = 0
+while menu != 1 and menu != 2:
+    menu = int(input('Menu\n=====\n1. Character\n2. Word: '))
+
+if menu == 1:
+    character = input('Enter a character: ')
+    word = character
+    type = "character"
+elif menu == 2:
     word = input('Enter a word: ')
-else:
-    sys.exit()
+    type = "word"
+    character = word  # Using the entire word as the value of character
 
-print('Results\n========')
+print('Results\n===========')
 
 results = get_results(word)
-print('Total: {0}'.format(results))
+print('The ASCII code of the {1} "{0}" is: {2}'.format(
+    character, type, results['ascii']))
+print('The binary code of "{0}" is: {1}'.format(
+    character, results['binary']))
